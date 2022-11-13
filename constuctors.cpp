75598455be22869f63cbf65c1af579572506b685
tt-213268 <takalkartejastt@gmail.com>
2022-11-13 19:32:22 +0100

    constructor:- added other employee

--- constuctors.cpp
+++ constuctors.cpp
@@ -25,4 +25,11 @@ int main()
     employee1.Company = "ARAI" ; 
     employee1.Name = "Tejas";
     employee1.IntroduceYourself();  
+
+    Employee employee2;  
+    employee1.Age = 23;    
+    employee1.Company = "Mahindra" ; 
+    employee1.Name = "John";
+    employee1.IntroduceYourself(); 
+
 }
