6d6f81aa94848d272579e5cab8fd1eb22f38b83a
tt-213268 <takalkartejastt@gmail.com>
2022-11-13 19:00:44 +0100

    classes_objecrs.py:-  using std::string evertime

--- classes_objects.cpp
+++ classes_objects.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
 
 class Employee {
+    std::string Name; 
+    std::string Company;  // we need to std::string everytime here
+
 
 };       // semi colon is essential
+
+int main()
+{
+
+}
