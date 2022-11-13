d59e5fc1d9c67af45b46a349fbb63f1c48b89f8f
tt-213268 <takalkartejastt@gmail.com>
2022-11-13 19:30:03 +0100

    created constructor.cpp

---
+++ constuctors.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using std::string; 
+
+
+
+
+class Employee {  
+public:              
+    string Name;    
+    string Company;  
+    int Age;
+
+    void IntroduceYourself(){                          
+        std::cout << "Name " << Name <<std::endl;
+        std::cout << "Company " << Company <<std::endl;
+        std::cout << "Age " << Age <<std::endl;
+    }
+};       
+
+int main()
+{
+    int number; 
+    Employee employee1;  
+    employee1.Age = 25;    
+    employee1.Company = "ARAI" ; 
+    employee1.Name = "Tejas";
+    employee1.IntroduceYourself();  
+}
