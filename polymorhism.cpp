24893696a48230b8ea0b5281ee9f268ce310d6b3
tt-213268 <takalkartejastt@gmail.com>
2022-11-13 22:55:15 +0100

    polymorphism:- implementation of polymorphism

--- polymorhism.cpp
+++ polymorhism.cpp
@@ -3,7 +3,12 @@ using std::string;
 using std::cout;
 
 /* 1. polymorphism - ability of object or method to represent multiple forms. in programing 
-2.
+2. We create a method in parent class and make it virtual
+3. virtual will check if there is a implementation of it in derived classes, if not it will run this implementation
+4. If there are different implementation of this in individual child classes- those methods will be implemented
+5. We create a pointer of the object of the parent class and store the referenc of the child class in it 
+6. we use e->work(); to run the method
+7. this can be used to collect data from different types of sensor with only few lines in for loop sensor[i]->sense() and they will gather data 
 */
 
 class AbstractEmployee{
@@ -18,7 +23,7 @@ public:
     string Company;  
     int Age;
 
-    void Work(){
+    virtual void Work(){         // virtual will check if there is a implementation of it in derived classes, if not it will run this implementation
 
         cout<< Name<<" is checking email, performing tasks " << std::endl;  //created a work method 
     }
@@ -91,8 +96,12 @@ int main()
     int number; 
     Developer d = Developer("Tejas","ARAI",25,"python");
     Teacher t = Teacher("Bob","cool school",35,"History");
-    t.Work();
-    d.Work();
+    
+    Employee *e1 = &d; // the pointer of employee will hold the reference of the developer as devloper is deep down inside an employee
+    Employee *e2 = &t; 
+
+    e1->Work();
+    e2->Work();    // this will work when we make the work method inside parent class vitual
 
 
 }
